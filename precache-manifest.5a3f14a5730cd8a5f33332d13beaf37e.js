d1936ff57c492688b04f05bc37a83f3d5166a415
sourabh chauhan <sourabh1202@gmail.com>
2020-08-22 15:27:58 +0530

    Updates

---
+++ precache-manifest.5a3f14a5730cd8a5f33332d13beaf37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3158840596d295d3619c8869ce159434",
+    "url": "/NeomorphicReactJSFormComponents/index.html"
+  },
+  {
+    "revision": "f96d1f72eb268caf4187",
+    "url": "/NeomorphicReactJSFormComponents/static/css/main.ffd5090c.chunk.css"
+  },
+  {
+    "revision": "a16d36728cd97d46aa06",
+    "url": "/NeomorphicReactJSFormComponents/static/js/2.59e65a06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NeomorphicReactJSFormComponents/static/js/2.59e65a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96d1f72eb268caf4187",
+    "url": "/NeomorphicReactJSFormComponents/static/js/main.b6aff87c.chunk.js"
+  },
+  {
+    "revision": "4f705879324156ed1225",
+    "url": "/NeomorphicReactJSFormComponents/static/js/runtime-main.79c6f3a0.js"
+  }
+]);
